720c99dc029cab418f6d4cb4700ff1fad67c4a9c
Nori Hayashi <net@hayashi-lin.net>
2024-08-05 02:58:55 +0900

    package: init

---
+++ iconic/bindings/c/tree-sitter-CategorialGrammarIconic.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_CATEGORIAL_GRAMMAR_ICONIC_H_
+#define TREE_SITTER_CATEGORIAL_GRAMMAR_ICONIC_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_CategorialGrammarIconic(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_CATEGORIAL_GRAMMAR_ICONIC_H_
---
+++ iconic/grammar.js
@@ -0,0 +1,89 @@
+module.exports = grammar({
+    name: "CategorialGrammarIconic",
+    extras: $ => [],
+    rules: {
+        source_file: $ => choice(
+            $.cat_funcapp,
+            $.cat_left,
+            $.cat_right,
+            $.cat_middle,
+            $.cat_uniq,
+            seq("<", $.source_file, ">", repeat($.feature_list)),
+        ),
+        cat_funcapp: $ => seq(
+            field("functor", $.source_file),
+            field("arguments", $.cat_functor_arg_list),
+            repeat($.feature_list),
+        ),
+        cat_functor_arg_list: $ => seq(
+            "(",
+            optional(
+                seq(
+                    $.source_file,
+                    repeat(seq(",", $.source_file)),
+                    optional(","),
+                )
+            ),
+            ")",
+        ),
+        cat_left: $ => prec.right(
+            seq(
+                field("antecedent", $.source_file),
+                "\\",
+                repeat($.feature_list),
+                field("consequence", $.source_file),
+            )
+        ),
+        cat_right: $ => prec.left(
+            seq(
+                field("consequence", $.source_file),
+                "/",
+                repeat($.feature_list),
+                field("antecedent", $.source_file),
+            )
+        ),
+        cat_middle: $ => prec.left(
+            seq(
+                field("consequence", $.source_file),
+                "|",
+                repeat($.feature_list),
+                field("antecedent", $.source_file),
+            )
+        ),
+        cat_uniq: $ => seq(
+            $.cat_name,
+            repeat($.feature_list),
+        ),
+        cat_name: $ => choice($.literal_unquoted, $.literal_quoted),
+        feature_list: $ => seq(
+            "[",
+            optional(
+                seq(
+                    $.feature,
+                    repeat(seq(",", $.feature)),
+                    optional(","),
+                )
+            ),
+            "]"
+        ),
+        feature: $ => seq(
+            field("key", choice($.literal_unquoted_feat, $.literal_quoted)),
+            optional(
+                seq(
+                    "=",
+                    field("value", choice($.literal_unquoted_feat, $.literal_quoted)),
+                )
+            ),
+        ),
+        // use ~ as the escape character
+        literal_unquoted: $ => /([^\\<>\[\]()'~,]|~.)+/,
+        literal_unquoted_feat: $ => /([^\\<>\[\]()'~,=]|~.)+/,
+        literal_quoted: $ => token(
+            seq(
+                "'",
+                /([^']|~')*/,
+                "'"
+            )
+        ),
+    }
+});
